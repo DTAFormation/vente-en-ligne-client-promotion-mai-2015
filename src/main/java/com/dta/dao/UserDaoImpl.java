81a026b78d6bd390c1428b079dbccc29797d3b1c
Sebastien Boureau <boureau.sebastien@gmail.com>
2015-05-04 09:40:52 +0200

    Update project skeleton. AngularJs added.

--- src/main/java/com/dta/dao/UserDaoImpl.java
+++ src/main/java/com/dta/dao/UserDaoImpl.java
@@ -2,6 +2,9 @@ package com.dta.dao;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +13,13 @@ import com.dta.domain.User;
 @Component("userDao")
 @Transactional
 public class UserDaoImpl implements UserDao {
+
+	private HibernateTemplate hibernateTemplate;
+
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		hibernateTemplate = new HibernateTemplate(sessionFactory);
+	}
 
 	@Override
 	public User create(User user) {

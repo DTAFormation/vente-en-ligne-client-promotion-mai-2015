6752eb075e2a255995100927d99f88d755f16461
Paul Bonnet <paulbnt@free.fr>
2015-05-05 15:08:25 +0200

    minor changes in views

--- src/main/webapp/app/js/controllers/HeaderController.js
+++ src/main/webapp/app/js/controllers/HeaderController.js
@@ -1,9 +1,12 @@
 angular.module("venteEnLigne")
-.controller("HeaderController", function() {
+.controller("HeaderController", function($location) {
 	var headerControl = this;
 	
 	//TODO: get the state of the connexion, login and all information from the cookies / localStorage or whereever it is stored
 	headerControl.connected = false;
 	headerControl.login = "toto";
-	
+
+	headerControl.currentPage = function() {
+		return $location.path();
+	}
 })

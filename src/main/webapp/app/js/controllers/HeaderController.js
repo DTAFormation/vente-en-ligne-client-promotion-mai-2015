357aa04998a058b0cde014c01654b69f576fc848
Paul Bonnet <paulbnt@free.fr>
2015-05-05 15:37:57 +0200

    bug fix in minor changes in views

--- src/main/webapp/app/js/controllers/HeaderController.js
+++ src/main/webapp/app/js/controllers/HeaderController.js
@@ -1,6 +1,6 @@
 angular.module("venteEnLigne")
 
-.controller("HeaderController", function($http, $scope, $modal) {
+.controller("HeaderController", function($http, $scope, $modal, $location) {
 	var headerControl = this;
 	
 	//TODO: get the state of the connexion, login and all information from the cookies / localStorage or whereever it is stored

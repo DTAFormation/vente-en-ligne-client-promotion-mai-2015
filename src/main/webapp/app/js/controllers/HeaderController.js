a217c109f37e94d52b97be025d8199c04d56305f
Paul Bonnet <paulbnt@free.fr>
2015-05-05 12:04:13 +0200

    real header created, small modifications on index.html

---
+++ src/main/webapp/app/js/controllers/HeaderController.js
@@ -0,0 +1,9 @@
+angular.module("venteEnLigne")
+.controller("HeaderController", function() {
+	var headerControl = this;
+	
+	//TODO: get the state of the connexion, login and all information from the cookies / localStorage or whereever it is stored
+	headerControl.connected = false;
+	headerControl.login = "toto";
+	
+})

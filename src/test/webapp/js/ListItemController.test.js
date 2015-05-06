6f9afe4d8504fd8a2a7b8c1912b7e644a79430c4
Thomas Legoahec <thomas.legoahec@gmail.com>
2015-05-06 17:54:00 +0200

    #28 Tests unitaire récupération articles

--- src/test/webapp/js/ListItemController.test.js
+++ src/test/webapp/js/ListItemController.test.js
@@ -3,22 +3,17 @@ describe('ListItemController', function() {
 		  module('venteEnLigne');
 	  });
 	  
-	  beforeEach(inject(function($rootScope, $controller){
+	  beforeEach(inject(function($rootScope, $controller,BasketService){
 	        //create an empty scope
 	        scope = $rootScope.$new();
 	        //declare the controller and inject our empty scope
 	        $controller('ListItemController', {$scope: scope});
+	        BasketService.deleteBasket();
 	    }));
 	    
 	   it('should change path to article/item.id', inject(function($rootScope, $controller,ItemService,$location) {
 		      scope.showItem({"articleId":"1","name":"toto","price":"15"});
 		      expect($location.path()).toEqual("/article/1");
 	   }));
-//	   
-//	   it('Basket should be equals to {"id":1, "name":"truc"}', inject(function($rootScope, $controller,BasketService) {
-//		      scope.addItemToBasket({"article_id":1, "name":"truc","price":"15"})
-//			  expect(localStorage.getItem('basket')).toEqual({"article_id":1, "name":"truc","price":"15","quantity":1});
-//		    }));
-	   
-	   
+   
 	});

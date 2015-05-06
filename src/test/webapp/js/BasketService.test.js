dd06d8a2d5e5b20ad087e3580b56dd9251299dec
Florent Berthelot <florent.berthelot@sfr.fr>
2015-05-06 16:12:50 +0200

    Update des tests pour BasketService #8

--- src/test/webapp/js/BasketService.test.js
+++ src/test/webapp/js/BasketService.test.js
@@ -8,9 +8,9 @@ describe('BasketService', function() {
 		expect(BasketService.getBasket()).toEqual([]);
 	}));
 
-	it('Basket should be equals to {"articleId":1, "name":"truc"} after an add', inject(function(BasketService) {
-		BasketService.addItemToBasket({"articleId":1, "name":"truc"});
-		expect(BasketService.getBasket()).toEqual([{"articleId":1, "name":"truc"}]);
+	it('Basket should be equals to {"entity":{"articleId":1, "name":"truc"}, "quantity":2} after an add', inject(function(BasketService) {
+		BasketService.addItemToBasket({"entity":{"articleId":1, "name":"truc"}, "quantity":2});
+		expect(BasketService.getBasket()).toEqual([{"entity":{"articleId":1, "name":"truc"}, "quantity":2}]);
 	}));
 	
 	it('Basket should be equals to [] after a delete', inject(function(BasketService) {
@@ -19,16 +19,16 @@ describe('BasketService', function() {
 	}));
 	
 	it('Basket should be equals to [] when a delete is after an add', inject(function(BasketService) {
-		BasketService.addItemToBasket({"articleId":1, "name":"truc"});
+		BasketService.addItemToBasket({"entity":{"articleId":1, "name":"truc"}, "quantity":2});
 		BasketService.deleteBasket();
 		expect(BasketService.getBasket()).toEqual([]);
 	}));
 
 	it('Basket should delete the item', inject(function(BasketService) {
-		BasketService.addItemToBasket({"articleId":1, "name":"truc"});
-		BasketService.addItemToBasket({"articleId":2, "name":"machin"});
-		BasketService.deleteItemFromBasket({"articleId":2, "name":"machin"})
-		expect(BasketService.getBasket()).toEqual([{"articleId":1, "name":"truc"}]);
+		BasketService.addItemToBasket({"entity":{"articleId":1, "name":"truc"}, "quantity":2});
+		BasketService.addItemToBasket({"entity":{"articleId":2, "name":"machin"}, "quantity":1});
+		BasketService.deleteItemFromBasket({"entity":{"articleId":2, "name":"machin"}, "quantity":1})
+		expect(BasketService.getBasket()).toEqual([{"entity":{"articleId":1, "name":"truc"}, "quantity":2}]);
 	}));
 	
 	

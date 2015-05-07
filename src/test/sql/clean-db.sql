e7fa600b702f75ef7ef43cdfad2b8abf0bfa8523
Florent Berthelot <florent.berthelot@sfr.fr>
2015-05-07 11:58:17 +0200

    Fix typographie sql

--- src/test/sql/clean-db.sql
+++ src/test/sql/clean-db.sql
@@ -1,8 +1,8 @@
 set foreign_key_checks = 0;
-truncate table adresse;
-truncate table article;
-truncate table catalogue;
-truncate table commande;
-truncate table produit;
-truncate table user;
+truncate table Adresse;
+truncate table Article;
+truncate table Catalogue;
+truncate table Commande;
+truncate table Produit;
+truncate table Utilisateur;
 set foreign_key_checks = 1;
--- src/test/sql/init-product-db.sql
+++ src/test/sql/init-product-db.sql
@@ -1,5 +1,5 @@
-insert into produit (produit_id, nom, description, catalogue_catalogue_id) values (1, 'football', 'liste de produits concernant le football', 1);
-insert into produit (produit_id, nom, description, catalogue_catalogue_id) values (2, 'basketball', 'liste de produits concernant le basketball', 1);
-insert into produit (produit_id, nom, description, catalogue_catalogue_id) values (3, 'cucurbitacé', 'liste de cucurbitacés', 2);
-insert into produit (produit_id, nom, description, catalogue_catalogue_id) values (4, 'tubercule', 'liste de tubercules', 2);
-insert into produit (produit_id, nom, description, catalogue_catalogue_id) values (5, 'fruits', 'liste de fruits', 2);
+insert into Produit (produit_id, nom, description, catalogue_catalogue_id) values (1, 'football', 'liste de produits concernant le football', 1);
+insert into Produit (produit_id, nom, description, catalogue_catalogue_id) values (2, 'basketball', 'liste de produits concernant le basketball', 1);
+insert into Produit (produit_id, nom, description, catalogue_catalogue_id) values (3, 'cucurbitacé', 'liste de cucurbitacés', 2);
+insert into Produit (produit_id, nom, description, catalogue_catalogue_id) values (4, 'tubercule', 'liste de tubercules', 2);
+insert into Produit (produit_id, nom, description, catalogue_catalogue_id) values (5, 'fruits', 'liste de fruits', 2);

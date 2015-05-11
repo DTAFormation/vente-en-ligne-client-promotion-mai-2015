88c0c1def1a284ad5621b5362eed507bb82098a5
Bastien Martinelli <contact.bmartinelli@gmail.com>
2015-05-11 17:53:29 +0200

    commit métier get cmds

--- src/main/java/com/dta/controller/CommandeController.java
+++ src/main/java/com/dta/controller/CommandeController.java
@@ -1,9 +1,16 @@
 package com.dta.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dta.domain.Commande;
+import com.dta.domain.Utilisateur;
 import com.dta.service.CommandeService;
 
 @RestController
@@ -12,5 +19,21 @@ public class CommandeController {
 	
 	@Autowired
 	private CommandeService commandeService;
+	
+	@RequestMapping(value="/{login}")
+	@ResponseBody
+	public UserJson getUser(@PathVariable("login") String login) {
+		
+		List<Commande> cmds = commandeService.getCommandeByLogin(login);
+		List<CommandeJson> cmdsJson = new ArrayList<CommandeJson>();
+		
+		for(Commande cmd : cmds){
+			cmdsJson.add(new CommandeJson(cmd));
+		}
+		
+		return null;
+	}
+	
+	
 
 }

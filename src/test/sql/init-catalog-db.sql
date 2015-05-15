4621f4913808f83b06f8ce156effd8b019442938
romain albisson <romain.albisson@polytech-lille.net>
2015-05-15 15:46:18 +0200

    Update init-catalog-db.sql

--- src/test/sql/init-catalog-db.sql
+++ src/test/sql/init-catalog-db.sql
@@ -1,2 +1,2 @@
-insert into Catalogue (nom, description) values ('sport', 'catalogue de sport');
-insert into Catalogue (nom, description) values ('primeurs', 'catalogue de primeurs');
+insert into Catalogue (catalogue_id, nom, description) values (1, 'sport', 'catalogue de sport');
+insert into Catalogue (catalogue_id, nom, description) values (2, 'primeurs', 'catalogue de primeurs');

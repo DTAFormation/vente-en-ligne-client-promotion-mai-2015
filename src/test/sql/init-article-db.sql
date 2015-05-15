d14414b0c8e1bc4fec1abd997d8f1ca992e3ba17
romain albisson <romain.albisson@polytech-lille.net>
2015-05-15 15:30:27 +0200

    Update init-article-db.sql

--- src/test/sql/init-article-db.sql
+++ src/test/sql/init-article-db.sql
@@ -1,11 +1,11 @@
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (1, 'ballon de foot', 'http://i.imgur.com/4O86daR.jpg', 10, 1, 25, 0.0, 0);
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (2, 'chaussures de foot', 'http://i.imgur.com/kG47bs8.jpg', 500, 1, 2000, 0.0, 0); 
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (3, 'ballon de basket', 'http://i.imgur.com/iDWMUTr.jpg', 15, 2, 30, 0.0, 0);
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (4, 'panier de basket', 'http://i.imgur.com/z3rOjE9.jpg', 100, 2, 30, 0.0, 0);
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (5, 'concombre', 'http://i.imgur.com/NUOJOdC.jpg', 2.35, 3, 10, 0.0, 0);
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (6, 'courgette', 'http://i.imgur.com/lhxWEQ3.jpg', 59.99, 3, 5000, 0.0, 0);
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (7, 'pomme de terre', 'http://i.imgur.com/vBRzDhq.jpg', 2.65, 4, 1000, 0.0, 0);
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (8, 'carotte', 'http://i.imgur.com/XhUhqXX.jpg', 3.05, 4, 60, 0.0, 0);
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (9, 'banane', 'http://i.imgur.com/BT2FVWT.jpg', 4.60, 5, 35, 0.0, 0);
-insert into Article (article_id, nom, image, prix, produit_produit_id, stock) VALUES (10, 'tomate', 'http://i.imgur.com/MQfZPWa.png', 4.70, 5, 25, 0.0, 0);
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (1, 'ballon de foot', 'http://i.imgur.com/4O86daR.jpg', 10, 1, 25, 0.0, 0);
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (2, 'chaussures de foot', 'http://i.imgur.com/kG47bs8.jpg', 500, 1, 2000, 0.0, 0); 
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (3, 'ballon de basket', 'http://i.imgur.com/iDWMUTr.jpg', 15, 2, 30, 0.0, 0);
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (4, 'panier de basket', 'http://i.imgur.com/z3rOjE9.jpg', 100, 2, 30, 0.0, 0);
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (5, 'concombre', 'http://i.imgur.com/NUOJOdC.jpg', 2.35, 3, 10, 0.0, 0);
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (6, 'courgette', 'http://i.imgur.com/lhxWEQ3.jpg', 59.99, 3, 5000, 0.0, 0);
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (7, 'pomme de terre', 'http://i.imgur.com/vBRzDhq.jpg', 2.65, 4, 1000, 0.0, 0);
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (8, 'carotte', 'http://i.imgur.com/XhUhqXX.jpg', 3.05, 4, 60, 0.0, 0);
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (9, 'banane', 'http://i.imgur.com/BT2FVWT.jpg', 4.60, 5, 35, 0.0, 0);
+insert into Article (article_id, nom, image, prix, produit_produit_id, stock, rating, nb_raters) VALUES (10, 'tomate', 'http://i.imgur.com/MQfZPWa.png', 4.70, 5, 25, 0.0, 0);
 

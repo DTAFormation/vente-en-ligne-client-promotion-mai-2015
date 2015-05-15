1e032399683c83c16f4cc486350c99afab14dd34
lrt <laurent.riviere09@gmail.com>
2015-05-15 11:31:46 +0200

    US303 #18 test karma

--- src/test/webapp/js/ConnectController.test.js
+++ src/test/webapp/js/ConnectController.test.js
@@ -1,11 +1,12 @@
 describe('ConnectController', function() {
 	beforeEach(function() {
 		module('venteEnLigne');
+		window.localStorage.removeItem("basket");
 	});
 
 	beforeEach(inject(function($rootScope, $controller){
 		scope = $rootScope.$new();
-		$controller('ConnectController', {$scope: scope});
+		$controller('ConnectController', {$scope: scope});		
 	}));
 
 	it("Should return the url '/signup'", inject(function($rootScope, $controller, $location) {
@@ -17,5 +18,23 @@ describe('ConnectController', function() {
 		scope.goToPurchase();
 		expect($location.path()).toEqual("/validatePayment");
 	}));
+	
+	// when the basket is empty before the connection
+	it("Should return the url '/'", inject(function($rootScope, $controller, $location, BasketService) {
+		scope.redirection();
+		expect($location.path()).toEqual("/");
+	}));
+	
+	// when the basket is not empty before the connection
+	it("Should return the url '/validatePayment'", inject(function($rootScope, $controller, $location, BasketService) {
+		BasketService.addItemToBasket({"entity":{"articleId":1, "name":"truc", "stock":20}, "quantity":2});
+		scope.redirection();
+		expect($location.path()).toEqual("/validatePayment");
+	}));
+	
+	
+	afterEach(function() {
+		window.localStorage.removeItem("basket");
+	});
 
 });

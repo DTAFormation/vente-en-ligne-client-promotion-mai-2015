8bcbba571375216f100833c7defef113290e2461
Bastien Martinelli <contact.bmartinelli@gmail.com>
2015-05-19 12:40:52 +0200

    modif history

--- src/main/webapp/app/js/controllers/HistoryController.js
+++ src/main/webapp/app/js/controllers/HistoryController.js
@@ -3,11 +3,11 @@ angular.module("venteEnLigne")
 .controller('HistoryController', function(ConnectService, CommandeService, $scope){
     
     $scope.cmds = [];
+    $scope.index;
 
     $scope.getCommands = function(){
         CommandeService.getCommande(ConnectService.getConnectedUser())
         .then(function(result){
-            console.log(result)
             $scope.cmds = result.data;
         });
     }
@@ -15,4 +15,25 @@ angular.module("venteEnLigne")
     angular.element(document).ready(function () {
         $scope.getCommands();
     });
+    
+    $scope.getTotalPrice = function(cl){
+    	var total = 0;
+    	console.log(cl)
+    	for (var key in cl) {
+    		console.log(key)
+    		  if (cl.hasOwnProperty(key)) {
+    		    total = total + cl[key].entity.price * cl[key].quantity
+    		 }
+    	}
+    	return total
+    }
+    
+    $scope.proccessShow = function(i){
+    	if(i == $scope.index){
+    		$scope.index = null
+    	}else{
+    		$scope.index = i;
+    	}
+    }
+
 })
